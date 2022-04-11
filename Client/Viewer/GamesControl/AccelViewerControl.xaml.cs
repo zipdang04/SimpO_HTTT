c1a8e5741d05b610037caba731782c723feea35a
Đặng Xuân Minh Hiếu <zipdang2004@gmail.com>
2022-04-11 18:18:01 +0700

    doing VCNV

---
+++ Client/Viewer/GamesControl/AccelViewerControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Server.Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.Viewer.GamesControl
+{
+	/// <summary>
+	/// Interaction logic for AccelControl.xaml
+	/// </summary>
+	public partial class AccelViewerControl : UserControl
+	{
+		public AccelViewerControl()
+		{
+			InitializeComponent();
+			mediaStart.Source = new Uri(HelperClass.PathString("Effects", "TT_Start.mp4"));
+			mediaStart.BeginInit(); mediaStart.Play(); mediaStart.Stop();
+			mediaRun.Source = new Uri(HelperClass.PathString("Effects", "TT_Run.mp4"));
+			mediaRun.BeginInit(); mediaRun.Play(); mediaRun.Stop();
+		}
+
+		public void Prepare(string question, string attach, int time)
+		{
+			txtQuestion.Visibility = Visibility.Hidden;
+			media.Source = new Uri(HelperClass.PathString("Media", attach));
+			media.Play(); media.Stop();
+			media.Volume = 0;
+
+			mediaStart.Position = TimeSpan.Zero; mediaStart.Play();
+		}
+
+		public void Run()
+		{
+		}
+
+		private void media_MediaEnded(object sender, RoutedEventArgs e)
+		{
+			txtQuestion.Visibility = Visibility.Visible;
+		}
+	}
+}
---
+++ Client/Viewer/GamesControl/Components/Letter.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Server.Information;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.Viewer.GamesControl.Components
+{
+	/// <summary>
+	/// Interaction logic for Letter.xaml
+	/// </summary>
+	public partial class Letter : UserControl
+	{
+		public enum LetterState { NORMAL, CHOOSING, ENABLE, DISABLE };
+		public static Uri NORMAL = new Uri(HelperClass.PathString("Effects", "VCNV_LetterNormal.png"));
+		public static Uri CHOOSING = new Uri(HelperClass.PathString("Effects", "VCNV_LetterChossing.png"));
+		public static Uri ENABLED = new Uri(HelperClass.PathString("Effects", "VCNV_LetterEnabled.png"));
+		public static Uri DISABLED = new Uri(HelperClass.PathString("Effects", "VCNV_LetterDisabled.png"));
+		public Letter()
+		{
+			InitializeComponent();
+			SetNormal();
+		}
+		public Letter(char c): this()
+		{
+			lblChar.Content = c;
+		}
+
+		public void SetNormal() { background.ImageSource = new BitmapImage(NORMAL); lblChar.Visibility = Visibility.Hidden; }
+		public void SetChoosing() { background.ImageSource = new BitmapImage(CHOOSING); lblChar.Visibility = Visibility.Hidden; }
+		public void SetEnabled() { background.ImageSource = new BitmapImage(ENABLED); lblChar.Visibility = Visibility.Hidden; }
+		public void SetDisabled() { background.ImageSource = new BitmapImage(DISABLED); lblChar.Visibility = Visibility.Hidden; }
+	}
+}
